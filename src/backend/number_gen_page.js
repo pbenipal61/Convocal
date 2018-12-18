eb1f99fe20a8151f3f0440921021d17afdfbdefd
Prabhjot Benipal <prabhjotbenipal@Prabhjots-MacBook-Pro.local>
2018-12-18 10:16:58 +0200

    Final changes to character_table page

--- src/backend/number_gen_page.js
+++ src/backend/number_gen_page.js
@@ -1,28 +1,51 @@
 var arr = [];
 function init() {
-    if (arr.length < 50) {
-        generate_num();
-    } else {
-        var output_str = `<table border="1"> <tr><th>Decimal</th><th>Binary</th><th>Octa</th><th>Hex</th></tr>`;
-        for (var i = 0; i < arr.length; i++) {
-            var res = arr[i];
-            output_str = output_str + `<tr><td>${res.decimal}</td><td>${res.binary}</td><td>${res.octa}</td><td>${res.hex}</td></tr>`;
+    try {
+        var min = document.getElementById("min").value;
+        var max = document.getElementById("max").value;
+        if (min.length > 0 && max.length > 0) {
+            if (arr.length < 100) {
+                generate_num();
+            } else {
+                var output_str = `<table border="1"> <tr><th>Decimal</th><th>Binary</th><th>Octa</th><th>Hex</th></tr>`;
+                for (var i = 0; i < arr.length; i++) {
+                    var res = arr[i];
+                    output_str = output_str + `<tr><td>${res.decimal}</td><td>${res.binary}</td><td>${res.octa}</td><td>${res.hex}</td></tr>`;
+                }
+                output_str = output_str + "</table>";
+                document.getElementById("output").innerHTML = output_str;
+            }
         }
-        output_str = output_str + "</table>";
-        document.getElementById("output").innerHTML = output_str;
+    }
+    catch{
+
+
     }
 }
 function generate_num() {
+    try {
+        var min = document.getElementById("min").value;
+        var max = document.getElementById("max").value;
+        if (min.length > 0 && max.length > 0) {
 
-    var n;
-    n = get_random_int(0, 100);
-    while (arr.indexOf(n) != -1) {
+            min = Number(min);
+            max = Number(max);
+            var n;
+            n = get_random_int(min, max);
+            while (arr.indexOf(n) != -1) {
 
-        n = get_random_int(0, 100);
+                n = get_random_int(0, 100);
+            }
+            arr.push(convert(n));
+
+            init();
+        }
     }
-    arr.push(convert(n));
+    catch{
 
-    init();
+    }
+
+
 
 
 
@@ -34,13 +57,25 @@ function get_random_int(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 function one_to_fifty_int() {
-    var output_str = `<table border="1"> <tr><th>Decimal</th><th>Binary</th><th>Octa</th><th>Hex</th></tr>`;
-    for (var i = 1; i < 51; i++) {
-        var res = convert(i);
-        output_str = output_str + `<tr><td>${res.decimal}</td><td>${res.binary}</td><td>${res.octa}</td><td>${res.hex}</td></tr>`;
+    try {
+        var min = document.getElementById("min").value;
+        var max = document.getElementById("max").value;
+        if (min.length > 0 && max.length > 0) {
+
+            min = Number(min);
+            max = Number(max);
+            var output_str = `<table border="1"> <tr><th>Decimal</th><th>Binary</th><th>Octa</th><th>Hex</th></tr>`;
+            for (var i = min; i < max + 1; i++) {
+                var res = convert(i);
+                output_str = output_str + `<tr><td>${res.decimal}</td><td>${res.binary}</td><td>${res.octa}</td><td>${res.hex}</td></tr>`;
+            }
+            output_str = output_str + "</table>";
+            document.getElementById("output").innerHTML = output_str;
+        }
     }
-    output_str = output_str + "</table>";
-    document.getElementById("output").innerHTML = output_str;
+    catch (e) {
+
+    }
 }
 function convert(inp) {
     console.log("converting " + inp);
